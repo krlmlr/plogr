098a7fba24c60836982e6dbf8340d6ccd8c879a3
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-23 18:57:11 +0200

    add package docs
    - Add package documentation with example.

---
+++ R/plogr-package.R
@@ -0,0 +1,19 @@
+#' @examples
+#' plogr_demo <- Rcpp::cppFunction(depends = "plogr", '
+#' // C++ code begin
+#' #include <plogr.h>
+#'
+#' RObject plogr_demo() {
+#'   plog::init_r(plog::info);
+#'   LOG_INFO << "shown";
+#'   LOG_DEBUG << "not shown";
+#'   return R_NilValue;
+#' }
+#'
+#' #include <Rcpp.h> // not necessary to use plogr
+#' // C++ code end
+#' '
+#' )
+#'
+#' plogr_demo()
+"_PACKAGE"
