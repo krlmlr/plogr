fc839793e5fbd53ee79951ec897f42dc1880a96b
Kirill Müller <krlmlr+r@mailbox.org>
2016-09-23 18:48:12 +0200

    Revert "use the simpler R_ShowMessage()"
    better use a documented method.
    
    This reverts commit a3445c8400456b3e5086f211b2716cdcb31be4d3.

--- inst/include/plogr.h
+++ inst/include/plogr.h
@@ -15,7 +15,7 @@ public:
   virtual void write(const Record& record)
   {
     util::nstring str = Formatter::format(record); // Use the formatter to get a string from a record.
-    R_ShowMessage(str.c_str());
+    REprintf("%s", str.c_str());
   }
 };
 
